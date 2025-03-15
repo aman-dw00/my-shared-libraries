a58c791025ea6d6006186b08bf7e48162df0bd11
aman-dw00 <aman.raj@mygurukulam.co>
2025-03-16 00:48:55 +0530

    Create code-compilation.groovy

---
+++ vars/code-compilation.groovy
@@ -0,0 +1,53 @@
+pipeline {
+    agent any
+
+    tools {
+        go 'Golang'  
+    }
+
+    environment {
+        REPORT_FILE = "compile_report.txt" 
+        branch_name = 'main'
+        repo_url = 'https://github.com/OT-MICROSERVICES/employee-api.git'
+        email_recipients = "aman.raj@mygurukulam.co"
+    }
+
+    stages {
+            stage('Clean Workspace') {
+            steps {
+                cleanWs()
+            }
+        }
+
+        stage('Clone Repository') {
+            steps {
+                    git url: repo_url, branch: branch_name, credentialsId: 'aman-git'
+            }
+        }
+
+        stage('Compile') {
+            steps {
+               
+                sh "go build "
+            }
+        }
+    }
+ post {
+        always {
+            script {
+                // Send an email after the pipeline is completed (regardless of success/failure)
+                emailext(
+                    subject: "Build Status - ${currentBuild.fullDisplayName}",
+                    body: """The build status for ${currentBuild.fullDisplayName} is as follows:
+
+                    Build Log URL: ${BUILD_URL}console
+
+                    Regards,
+                    Jenkins
+                    """,
+                    to: email_recipients
+                )
+            }
+        }
+    }
+}
