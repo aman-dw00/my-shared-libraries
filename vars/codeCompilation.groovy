654afc17e46a91a29dae3fe9dad8636d83d84f73
aman-dw00 <aman.raj@mygurukulam.co>
2025-03-16 01:13:09 +0530

    Update codeCompilation.groovy

--- vars/codeCompilation.groovy
+++ vars/codeCompilation.groovy
@@ -1,18 +1,14 @@
 def call() {
-    stage('Clean Workspace') {
-        steps {
+    script {
+        stage('Clean Workspace') {
             cleanWs()
         }
-    }
 
-    stage('Clone Repository') {
-        steps {
+        stage('Clone Repository') {
             git url: 'https://github.com/OT-MICROSERVICES/employee-api.git', branch: 'main', credentialsId: 'aman-git'
         }
-    }
 
-    stage('Compile') {
-        steps {
+        stage('Compile') {
             sh "go build"
         }
     }
