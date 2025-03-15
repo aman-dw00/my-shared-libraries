a0f45e43395b33e7d27b7251a064e4616091d827
aman-dw00 <aman.raj@mygurukulam.co>
2025-03-16 01:09:25 +0530

    Update codeCompilation.groovy

--- vars/codeCompilation.groovy
+++ vars/codeCompilation.groovy
@@ -1,53 +1,36 @@
 def call() {
-    pipeline {
-        agent any
-
-        tools {
-            go 'Golang'  
+    stage('Clean Workspace') {
+        steps {
+            cleanWs()
         }
+    }
 
-        environment {
-            REPORT_FILE = "compile_report.txt"
-            branch_name = 'main'
-            repo_url = 'https://github.com/OT-MICROSERVICES/employee-api.git'
-            email_recipients = "aman.raj@mygurukulam.co"
+    stage('Clone Repository') {
+        steps {
+            git url: 'https://github.com/OT-MICROSERVICES/employee-api.git', branch: 'main', credentialsId: 'aman-git'
         }
+    }
 
-        stages {
-            stage('Clean Workspace') {
-                steps {
-                    cleanWs()
-                }
-            }
-
-            stage('Clone Repository') {
-                steps {
-                    git url: repo_url, branch: branch_name, credentialsId: 'aman-git'
-                }
-            }
-
-            stage('Compile') {
-                steps {
-                    sh "go build"
-                }
-            }
+    stage('Compile') {
+        steps {
+            sh "go build"
         }
+    }
 
-        post {
-            always {
-                script {
-                    emailext(
-                        subject: "Build Status - ${currentBuild.fullDisplayName}",
-                        body: """The build status for ${currentBuild.fullDisplayName} is as follows:
+    post {
+        always {
+            script {
+                emailext(
+                    subject: "Build Status - ${currentBuild.fullDisplayName}",
+                    body: """The build status for ${currentBuild.fullDisplayName} is as follows:
 
-                        Build Log URL: ${BUILD_URL}console
+                    Build Log URL: ${BUILD_URL}console
 
-                        Regards,
-                        Jenkins
-                        """,
-                        to: email_recipients
-                    )
-                }
+                    Regards,
+                    Jenkins
+                    """,
+                    to: "aman.raj@mygurukulam.co"
+                )
             }
         }
     }
