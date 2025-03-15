1305aef0d384a820ef43b6750ebb185e6893111f
aman-dw00 <aman.raj@mygurukulam.co>
2025-03-16 00:56:00 +0530

    Update code-compilation.groovy

--- vars/code-compilation.groovy
+++ vars/code-compilation.groovy
@@ -1,52 +1,53 @@
-pipeline {
-    agent any
+def call(Map conf = [:]) {
+    pipeline {
+        agent any
 
-    tools {
-        go 'Golang'  
-    }
+        tools {
+            go 'Golang'  
+        }
 
-    environment {
-        REPORT_FILE = "compile_report.txt" 
-        branch_name = 'main'
-        repo_url = 'https://github.com/OT-MICROSERVICES/employee-api.git'
-        email_recipients = "aman.raj@mygurukulam.co"
-    }
+        environment {
+            REPORT_FILE = "compile_report.txt"
+            branch_name = conf.branch ?: 'main'
+            repo_url = conf.repo ?: 'https://github.com/OT-MICROSERVICES/employee-api.git'
+            email_recipients = config.email ?: "aman.raj@mygurukulam.co"
+        }
 
-    stages {
+        stages {
             stage('Clean Workspace') {
-            steps {
-                cleanWs()
+                steps {
+                    cleanWs()
+                }
             }
-        }
 
-        stage('Clone Repository') {
-            steps {
+            stage('Clone Repository') {
+                steps {
                     git url: repo_url, branch: branch_name, credentialsId: 'aman-git'
+                }
+            }
+
+            stage('Compile') {
+                steps {
+                    sh "go build"
+                }
             }
         }
 
-        stage('Compile') {
-            steps {
-               
-                sh "go build "
-            }
-        }
-    }
- post {
-        always {
-            script {
-                // Send an email after the pipeline is completed (regardless of success/failure)
-                emailext(
-                    subject: "Build Status - ${currentBuild.fullDisplayName}",
-                    body: """The build status for ${currentBuild.fullDisplayName} is as follows:
+        post {
+            always {
+                script {
+                    emailext(
+                        subject: "Build Status - ${currentBuild.fullDisplayName}",
+                        body: """The build status for ${currentBuild.fullDisplayName} is as follows:
 
-                    Build Log URL: ${BUILD_URL}console
+                        Build Log URL: ${BUILD_URL}console
 
-                    Regards,
-                    Jenkins
-                    """,
-                    to: email_recipients
-                )
+                        Regards,
+                        Jenkins
+                        """,
+                        to: email_recipients
+                    )
+                }
             }
         }
     }
